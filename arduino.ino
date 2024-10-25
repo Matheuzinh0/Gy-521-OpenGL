02c36d1dfa2e93e9554e5f15c52e917d0734b43c
Matheus Sousa <132166437+Matheuzinh0@users.noreply.github.com>
2024-10-25 17:38:53 -0300

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -8,10 +8,9 @@ void setup() {
   Serial.begin(115200);
   Wire.begin();
 
-  // Inicializa o MPU6050
   if (!mpu.begin()) {
     Serial.println("Não foi possível encontrar um MPU6050!");
-    while (1); // Fica preso se não conectar
+    while (1);
   }
 
   mpu.setGyroRange(MPU6050_RANGE_2000_DEG);
@@ -23,11 +22,9 @@ void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  // Calcular os ângulos de pitch e roll
   float pitch = atan2(a.acceleration.y, a.acceleration.z) * 180.0 / PI;
   float roll = atan2(a.acceleration.x, a.acceleration.z) * 180.0 / PI;
-
-  // Enviar os ângulos via Serial
+  
   Serial.print(pitch);
   Serial.print(",");
   Serial.println(roll);
